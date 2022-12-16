bc4530d2bc0827d324a8c3667cd3cd0733b93e8b
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-16 07:18:49 +0530

    Reminders were fetched in the app component and added some styling classes to the list items.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
+import reminderService from "./services/reminder";
 import "./App.css";
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([{ id: 1, title: "Reminder 1" }]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       let reminders = await reminderService.getReminders();
+  //       setReminders(reminders);
+  //     };
+  //     fetchData();
+  //   }, []);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    let reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
 
   return (
     <div className="App">
